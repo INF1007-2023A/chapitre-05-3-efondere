3f4df9ad90f7180674e3e8808857075e98544d0c
efondere <97473864+efondere@users.noreply.github.com>
2023-09-24 16:38:44 -0400

    terminer l'exercice du chapitre 5.3

--- exercice.py
+++ exercice.py
@@ -4,21 +4,57 @@
 import math
 
 def get_num_letters(text):
-	return 0
+	sum = 0
+	for c in text:
+		if c.isalnum():
+			sum += 1
+	return sum
 
 def get_word_length_histogram(text):
-	return [0]
+	length_data = []
+	longest = 0
+	for word in text.split():
+		length = get_num_letters(word)
+		length_data.append(length)
+		if length > longest:
+			longest = length
+
+	histogram_data = [0 for _ in range(longest + 1)]
+	for length in length_data:
+		histogram_data[length] += 1
+
+	return histogram_data
 
 def format_histogram(histogram):
 	ROW_CHAR = "*"
+	alignment = len(str(len(histogram) - 1))
+	result = ""
+	for i in range(1, len(histogram)):
+		histogram_bar = ROW_CHAR * histogram[i]
+		result += f"{i:>{alignment}} {histogram_bar}\n"
 
-	return ""
+	return result
 
 def format_horizontal_histogram(histogram):
 	BLOCK_CHAR = "|"
 	LINE_CHAR = "¯"
 
-	return ""
+	max_value = 0
+	for value in histogram:
+		if value > max_value:
+			max_value = value
+
+	result = ""
+	for y in range(max_value, 0, -1):
+		for x in range(1, len(histogram)):
+			if histogram[x] >= y:
+				result += BLOCK_CHAR
+			else:
+				result += " "
+		result += "\n"
+
+	result += LINE_CHAR * len(histogram)
+	return result
 
 
 if __name__ == "__main__":
